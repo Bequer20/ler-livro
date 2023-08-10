12d944749e121ce12f52e4a7367390d391c9f277
Bequer <n.bequer19@gmail.com>
2023-08-10 12:25:03 -0300

    criaturas1

--- ler-livro.py
+++ ler-livro.py
@@ -1,18 +1,11 @@
 import PyPDF2 as ler
 import pandas as pd
+import re
 
-def remover_palavras(texto, palavras):
+def excluir_palavras(texto, palavras):
     for palavra in palavras:
         texto = texto.replace(palavra, '')
     return texto
-
-def extrair_linhas(texto):
-    linhas = texto.strip().split('\n')
-    linhas_limpas = []
-    for linha in linhas:
-        if linha.strip():
-            linhas_limpas.append(linha.strip())
-    return linhas_limpas
 
 pdf_file = open(r"C:\\Users\\nah26\\Downloads\\fonte.pdf", 'rb')
 dados_pdf = ler.PdfReader(pdf_file)
@@ -26,26 +19,17 @@ indice2 = dados_pdf.pages[4]
 conteudo_indice1 = indice1.extract_text()
 conteudo_indice2 = indice2.extract_text()
 
-palavras = ('I-TableofContents', 'TableofContents', 'Credits III', 'Introduction 1', 'TableofContents-II','AppendixA:','AppendixB:','AppendixC:HirelingCosts','AppendixD:','AppendixE:Conditions')
-conteudo_indice1 = remover_palavras(conteudo_indice1, palavras)
-conteudo_indice2 = remover_palavras(conteudo_indice2, palavras)
+palavras_exclusao = ('I-TableofContents', 'TableofContents', 'Credits III', 'Introduction 1', 'TableofContents-II','AppendixA:','AppendixB:','AppendixC:HirelingCosts','AppendixD:','AppendixE:Conditions')
 
-def criar_dataframe(conteudo):
-    linhas = extrair_linhas(conteudo)
-    criaturas = []
+conteudo_indice1 = excluir_palavras(conteudo_indice1, palavras_exclusao)
+conteudo_indice2 = excluir_palavras(conteudo_indice2, palavras_exclusao)
 
-    for linha in linhas:
-        partes = linha.split()
-        if len(partes) >= 2:
-            nome = ' '.join(partes[:-1])
-            pagina = partes[-1]
-            criaturas.append({'Nome': nome, 'Pagina': pagina})
+pattern = r"([A-Za-z\s]+)(\d+)"
+matches_indice1 = re.findall(pattern, conteudo_indice1)
+matches_indice2 = re.findall(pattern, conteudo_indice2)
 
-    return pd.DataFrame(criaturas)
+criaturas = [{'Nome': match[0].strip(), 'Pagina': match[1]} for match in matches_indice1 + matches_indice2 if match[0].strip()]
 
-df1 = criar_dataframe(conteudo_indice1)
-df2 = criar_dataframe(conteudo_indice2)
+df = pd.DataFrame(criaturas)
 
-df = pd.concat([df1, df2], ignore_index=True)
-
-print(df.to_string())
+print(df.to_string(index=False))
